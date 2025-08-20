789d6d7631de7ab7c411316a389327e033959e0b
Yofi Dewo Prayogo <95621647+YofiDewoPrayogo@users.noreply.github.com>
2025-08-20 21:27:07 +0700

    database terbaru

--- db_gudang.sql
+++ db_gudang.sql
@@ -1,92 +1,352 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 20, 2025 at 04:18 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_gudang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
 CREATE TABLE `categories` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(2, 'OFFICE EQUIPMENTS'),
+(3, 'SPARE PART'),
+(4, 'KENDARAAN DAN ALAT BERAT'),
+(5, 'tes');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `incoming_items`
+--
+
+CREATE TABLE `incoming_items` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `purchase_id` int(11) UNSIGNED NOT NULL,
+  `product_id` int(11) UNSIGNED NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `incoming_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `incoming_items`
+--
+
+INSERT INTO `incoming_items` (`id`, `purchase_id`, `product_id`, `quantity`, `incoming_date`) VALUES
+(11, 2, 1, 1, '2025-08-20 12:02:42'),
+(12, 3, 1, 12, '2025-08-20 12:05:56'),
+(13, 4, 1, 11, '2025-08-20 12:08:58');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `outgoing_items`
+--
+
+CREATE TABLE `outgoing_items` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `product_id` int(11) UNSIGNED NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `outgoing_date` datetime NOT NULL,
+  `notes` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `outgoing_items`
+--
+
+INSERT INTO `outgoing_items` (`id`, `product_id`, `quantity`, `outgoing_date`, `notes`) VALUES
+(1, 1, 2, '2025-08-20 11:11:37', 'tes aja'),
+(2, 1, 30, '2025-08-20 12:36:02', 'tes stok');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
 
 CREATE TABLE `products` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` int(11) UNSIGNED NOT NULL,
   `category_id` int(11) UNSIGNED NOT NULL,
   `code` varchar(50) NOT NULL,
   `name` varchar(150) NOT NULL,
   `unit` varchar(50) NOT NULL,
-  `stock` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `code` (`code`),
-  KEY `products_category_id_foreign` (`category_id`),
-  CONSTRAINT `products_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `stock` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
-CREATE TABLE `vendors` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(150) NOT NULL,
-  `address` text DEFAULT NULL,
-  `phone` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `category_id`, `code`, `name`, `unit`, `stock`) VALUES
+(1, 5, '123', 'tas', 'pcs', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchases`
+--
 
 CREATE TABLE `purchases` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` int(11) UNSIGNED NOT NULL,
   `vendor_id` int(11) UNSIGNED NOT NULL,
   `purchase_date` date NOT NULL,
   `buyer_name` varchar(100) NOT NULL,
   `total_amount` decimal(15,2) NOT NULL DEFAULT 0.00,
-  `status` enum('Pending','Completed') NOT NULL DEFAULT 'Pending',
-  PRIMARY KEY (`id`),
-  KEY `purchases_vendor_id_foreign` (`vendor_id`),
-  CONSTRAINT `purchases_vendor_id_foreign` FOREIGN KEY (`vendor_id`) REFERENCES `vendors` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `status` enum('Pending','Completed') NOT NULL DEFAULT 'Pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `purchases`
+--
+
+INSERT INTO `purchases` (`id`, `vendor_id`, `purchase_date`, `buyer_name`, `total_amount`, `status`) VALUES
+(2, 1, '2025-08-20', 'Administrator', 10000.00, 'Completed'),
+(3, 1, '2025-08-20', 'Administrator', 120000.00, 'Completed'),
+(4, 1, '2025-08-20', 'Administrator', 110000.00, 'Completed');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchase_details`
+--
 
 CREATE TABLE `purchase_details` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` int(11) UNSIGNED NOT NULL,
   `purchase_id` int(11) UNSIGNED NOT NULL,
   `product_id` int(11) UNSIGNED NOT NULL,
   `quantity` int(11) NOT NULL,
-  `price` decimal(15,2) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `purchase_details_purchase_id_foreign` (`purchase_id`),
-  KEY `purchase_details_product_id_foreign` (`product_id`),
-  CONSTRAINT `purchase_details_product_id_foreign` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `purchase_details_purchase_id_foreign` FOREIGN KEY (`purchase_id`) REFERENCES `purchases` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `price` decimal(15,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
+-- Dumping data for table `purchase_details`
+--
 
-CREATE TABLE `incoming_items` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `purchase_id` int(11) UNSIGNED NOT NULL,
-  `product_id` int(11) UNSIGNED NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `incoming_date` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `incoming_items_purchase_id_foreign` (`purchase_id`),
-  KEY `incoming_items_product_id_foreign` (`product_id`),
-  CONSTRAINT `incoming_items_product_id_foreign` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
-  CONSTRAINT `incoming_items_purchase_id_foreign` FOREIGN KEY (`purchase_id`) REFERENCES `purchases` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO `purchase_details` (`id`, `purchase_id`, `product_id`, `quantity`, `price`) VALUES
+(1, 2, 1, 1, 10000.00),
+(2, 3, 1, 12, 10000.00),
+(3, 4, 1, 11, 10000.00);
 
-CREATE TABLE `outgoing_items` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `product_id` int(11) UNSIGNED NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `outgoing_date` datetime NOT NULL,
-  `notes` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `outgoing_items_product_id_foreign` (`product_id`),
-  CONSTRAINT `outgoing_items_product_id_foreign` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- --------------------------------------------------------
 
--- Insert Contoh Kategori
-INSERT INTO `categories` (`id`, `name`) VALUES
-(1, 'IT EQUIPMENTS'),
-(2, 'OFFICE EQUIPMENTS'),
-(3, 'SPARE PART'),
-(4, 'KENDARAAN DAN ALAT BERAT');
+--
+-- Table structure for table `users`
+--
 
 CREATE TABLE `users` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` int(11) UNSIGNED NOT NULL,
   `name` varchar(100) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `username`, `password`) VALUES
+(1, 'Administrator', 'admin', '$2y$10$iF/fM90E519A3Wavg.CgPuupoPSsf06EVE0PnQ7Cuk4kgmflDt8VK');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vendors`
+--
+
+CREATE TABLE `vendors` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `address` text DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `vendors`
+--
+
+INSERT INTO `vendors` (`id`, `name`, `address`, `phone`) VALUES
+(1, 'kuon', 'jl pahlawan kerja', '081929491887'),
+(2, 'gacoan', 'jl panam', '089528564469');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `incoming_items`
+--
+ALTER TABLE `incoming_items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `incoming_items_purchase_id_foreign` (`purchase_id`),
+  ADD KEY `incoming_items_product_id_foreign` (`product_id`);
+
+--
+-- Indexes for table `outgoing_items`
+--
+ALTER TABLE `outgoing_items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `outgoing_items_product_id_foreign` (`product_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `code` (`code`),
+  ADD KEY `products_category_id_foreign` (`category_id`);
+
+--
+-- Indexes for table `purchases`
+--
+ALTER TABLE `purchases`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `purchases_vendor_id_foreign` (`vendor_id`);
+
+--
+-- Indexes for table `purchase_details`
+--
+ALTER TABLE `purchase_details`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `purchase_details_purchase_id_foreign` (`purchase_id`),
+  ADD KEY `purchase_details_product_id_foreign` (`product_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `vendors`
+--
+ALTER TABLE `vendors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `incoming_items`
+--
+ALTER TABLE `incoming_items`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `outgoing_items`
+--
+ALTER TABLE `outgoing_items`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `purchases`
+--
+ALTER TABLE `purchases`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `purchase_details`
+--
+ALTER TABLE `purchase_details`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `vendors`
+--
+ALTER TABLE `vendors`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `incoming_items`
+--
+ALTER TABLE `incoming_items`
+  ADD CONSTRAINT `incoming_items_product_id_foreign` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+  ADD CONSTRAINT `incoming_items_purchase_id_foreign` FOREIGN KEY (`purchase_id`) REFERENCES `purchases` (`id`);
+
+--
+-- Constraints for table `outgoing_items`
+--
+ALTER TABLE `outgoing_items`
+  ADD CONSTRAINT `outgoing_items_product_id_foreign` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `purchases`
+--
+ALTER TABLE `purchases`
+  ADD CONSTRAINT `purchases_vendor_id_foreign` FOREIGN KEY (`vendor_id`) REFERENCES `vendors` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `purchase_details`
+--
+ALTER TABLE `purchase_details`
+  ADD CONSTRAINT `purchase_details_product_id_foreign` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `purchase_details_purchase_id_foreign` FOREIGN KEY (`purchase_id`) REFERENCES `purchases` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
